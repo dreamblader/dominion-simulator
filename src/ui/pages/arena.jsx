cc6ea6df9952042634a1b80b1ca24a3c85e6cffa
Carlos Gabriel Monnazzi <carlos.monnazzi@datum.inf.br>
2021-09-16 21:19:53 -0300

    menu style fix, click still not work

--- src/ui/pages/arena.jsx
+++ src/ui/pages/arena.jsx
@@ -3,19 +3,20 @@ import Card from "../components/card"
 import Jar from "../components/jar"  
 import Hand from "../components/hand";
 import Menu from "../components/menu";
-import { getDeckActionsOnMenu } from "../../actions/deck";  
 import "../styles/arena.css"
 
 
 const Arena = (props) => {
     let deckMenu = () => {
-        props.moves.getDeckActionsOnMenu()
+        console.log("here");
+        props.moves.getDeckActionsOnMenu();
     }; 
+
     return (
     <div className="arena">
         {props.G.menu && <Menu items={props.G.menu.actions}/>}
         <div className="deck-col">
-            <Card onClick={() => console.log("yo")}>{props.G.deck[1].length} +1</Card>
+            <Card onClick={() => deckMenu}>{props.G.deck[1].length} +1</Card>
             <Card>{props.G.destroyZone[1].length}</Card>
             <Jar>OUT</Jar>
             <Card>{props.G.destroyZone[0].length}</Card>
