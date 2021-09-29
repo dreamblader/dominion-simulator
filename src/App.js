01faf6dcbdb86fe1e9110e76e18c4317f335b8f2
Carlos Gabriel Monnazzi <carlos.monnazzi@datum.inf.br>
2021-09-29 18:28:58 -0300

    added server + lobby - need to refactor lobby

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-//TODO default doc code -- change this
-import { Client } from "boardgame.io/react";
-//import { SocketIO } from 'boardgame.io/multiplayer'
+import { Client, Lobby } from "boardgame.io/react";
+import { SocketIO } from "boardgame.io/multiplayer";
 import { Simulator } from "./Game";
 import Arena from "./ui/pages/arena";
 import "./ui/styles/globals.css";
+import { useState } from "react";
 
 const SimulatorClient = Client({
   game: Simulator,
   board: Arena,
   debug: false,
-  //multiplayer: SocketIO({ server: 'localhost:8000' }),
+  multiplayer: SocketIO({ server: "localhost:8000" }),
 });
 
-const App = SimulatorClient; //<****Client> in React
+const lobby = (
+  <Lobby
+    gameServer={`https://${window.location.hostname}:8000`}
+    lobbyServer={`https://${window.location.hostname}:8000`}
+    gameComponents={[{ game: Simulator, board: Arena }]}
+  />
+);
+
+const App = () => {
+  let { playerID, setPlayerID } = useState(null);
+
+  if (playerID) {
+    return <SimulatorClient playerID={playerID} />;
+  } else {
+    return lobby;
+  }
+};
 
 export default App;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
-//TODO default doc code -- change this
-const { Server, Origins } = require('boardgame.io/server');
-const { Simulator } = require('./Game');
+const { Server, Origins } = require("boardgame.io/server");
+const { Simulator } = require("./Game");
+
+const lobbyConfig = {
+  apiPort: 8080,
+  apiCallback: () => console.log("Running Lobby API on port 8080..."),
+};
 
 const server = Server({
   games: [Simulator],
   origins: [Origins.LOCALHOST],
 });
 
-server.run(8000);
+server.run({ port: 8000, lobbyConfig });
